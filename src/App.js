5ed573a77b57d00f00a6bba5ad8bf1ad630d8bd4
Gabrielbemo <gabrielbemo@gmail.com>
2018-07-27 21:41:10 -0300

    adicionado scroll para a pagina de robos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll'
 import './App.css';
 
 
@@ -9,29 +9,36 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
 
-render() {
+    render() {
 
-    const filterRobots = this.state.robots.filter(robots => {
-        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
+        const filterRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
 
-    return (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filterRobots} />
-        </div>
-    );
-}
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filterRobots} />
+                </Scroll>
+            </div>
+        );
+    }
 }
 
 
